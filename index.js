fd47b7a85ae6ba238762a7e77b4bb774221e0561
Gino Sebastiani <ginosebastiani@live.com>
2022-03-12 18:29:40 -0500

    commit tree b1515df680af8551a72458020bf9b403c8424eee

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    if (block > 42)
+    return block - 42
+    if (block < 42)
+    return 42 - block
+}
+
+function distanceFromHqInFeet(block){
+    let distanceInFeet = distanceFromHqInBlocks(block) * 264;
+    return distanceInFeet
+}
+
+function distanceTravelledInFeet(start,destination){
+    if (start < destination)
+    return (destination - start) * 264;
+    if (destination < start)
+    return (start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination){
+    let feetTraveled = distanceTravelledInFeet(start, destination)
+    
+    console.log (feetTraveled)
+    if (feetTraveled < 400){
+        return 0
+    } else if (feetTraveled < 2000){
+        return (feetTraveled - 400) * .02;
+    } else if (feetTraveled < 2500){
+        return 25;
+    } else{
+        return "cannot travel that far";
+    }
+}
